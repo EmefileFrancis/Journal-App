38c6d4860b8dfc55f72c6e3f7884f4a99699b0ec
Francis Emefile <emefilefranklin@gmail.com>
2018-06-25 19:34:47 +0100

    Initial Commit

---
+++ app/src/main/java/com/emefilefrancis/journalapp/AddJournalActivity.java
@@ -0,0 +1,13 @@
+package com.emefilefrancis.journalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddJournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_journal);
+    }
+}
